a6d634ccb2282e0ad21bdbf71b45a0b8c2808881
“Umair <ubsheikh.4@gmail.com>
2020-09-13 22:50:57 +0100

    Updating Contacts- Updated Open Modal to Load Contact Data

--- contact-list-react/src/App.js
+++ contact-list-react/src/App.js
@@ -101,7 +101,19 @@ class App extends React.Component {
     if (event) event.preventDefault();
     let contactId = event.target.value;
     console.log(`Editing contact id ${contactId}`)
-    this.setState({ showEditModal: true })
+    // submit a GET request to the /contact/{contactId} endpoint
+    // the response should come back with the associated contact's JSON
+    fetch(SERVICE_URL + '/contact/' + contactId)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        this.setState(
+          { editContactData: data, showEditModal: true }
+        )
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
 
   render() {
