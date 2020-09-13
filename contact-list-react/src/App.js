16ad2283c2815b1aaaeef16497e02100408b115c
“Umair <ubsheikh.4@gmail.com>
2020-09-13 19:35:19 +0100

    React Contact List : Loading Contacts-Changed App to Class Component and Add State

--- contact-list-react/src/App.js
+++ contact-list-react/src/App.js
@@ -7,27 +7,42 @@ import ContactModal from "./components/ContactModal"
 import { Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <h1 className="text-center">Contact Application</h1>
-        </Col>
-      </Row>
-      <hr />
-      <Row>
-        <Col sm={8}>
-          <h2>My Contacts</h2>
-          <ContactTable />
-        </Col>
-        <Col sm={4}>
-          <h2>Add New Contact</h2>
-          <ContactForm />
-        </Col>
-      </Row>
-      {/* <ContactModal /> */}
-    </Container>
-  );
+class App extends React.Component {
+  state = {
+    loading: false,
+    contactData: [
+      {
+        "contactId": 1, "firstName": "Fake",
+        "lastName": "Data",
+        "company": "Unknown Inc.",
+        "phone": "000-0000",
+        "email": "fakedata@unknown.io"
+      }]
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>
+            <h1 className="text-center">Contact Application</h1>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col sm={8}>
+            <h2>My Contacts</h2>
+            <ContactTable contacts={this.state.contactData} />
+            <ContactTable />
+          </Col>
+          <Col sm={4}>
+            <h2>Add New Contact</h2>
+            <ContactForm />
+          </Col>
+        </Row>
+        {/* <ContactModal /> */}
+      </Container >
+    );
+  }
 }
 export default App;
