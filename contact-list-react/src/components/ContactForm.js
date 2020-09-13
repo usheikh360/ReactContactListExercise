cef2771989e7ad1f710720a5ca53dc0860cad357
“Umair <ubsheikh.4@gmail.com>
2020-09-13 20:39:02 +0100

    Creating the Contact Form

--- contact-list-react/src/components/ContactForm.js
+++ contact-list-react/src/components/ContactForm.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { Form, Button } from 'react-bootstrap'
+class ContactForm extends React.Component {
 
-function ContactForm() {
-    return (
-        <h1>Contact Form</h1>
-    )
+    render() {
+        return (
+            <Form>
+                <Form.Group controlId="contactFirstName">
+                    <Form.Label>First Name:</Form.Label>
+                    <Form.Control type="text" placeholder="First Name" name="firstName" />
+                </Form.Group>
+                <Form.Group controlId="contactLastName">
+                    <Form.Label>Last Name:</Form.Label>
+                    <Form.Control type="text" placeholder="Last Name" name="lastName" />
+                </Form.Group>
+                <Form.Group controlId="contactCompany">
+                    <Form.Label>Company:</Form.Label>
+                    <Form.Control type="text" placeholder="Company" name="company" />
+                </Form.Group>
+                <Form.Group controlId="contactPhone">
+                    <Form.Label>Phone Number:</Form.Label>
+                    <Form.Control type="phone" placeholder="Phone Number" name="phone" />
+                </Form.Group>
+                <Form.Group controlId="contactEmail">
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control type="email" placeholder="Email" name="email" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        )
+    }
 }
 
 export default ContactForm
