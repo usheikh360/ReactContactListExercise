1ca7f4087264809f1a2aabbd0307dc795903bd1f
“Umair <ubsheikh.4@gmail.com>
2020-09-13 19:02:44 +0100

    React Contact List : Creating the Contact Table - includes hard coded data in the table at the moment

--- contact-list-react/src/components/ContactTable.js
+++ contact-list-react/src/components/ContactTable.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Table, Button } from 'react-bootstrap'
 
-function ContactTable() {
-    return (
-        <h1>Contact Table</h1>
-    )
+class ContactTable extends React.Component {
+    Ssuper(props) {
+
+    }
+
+    render() {
+        return (<Table striped bordered responsive>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Company</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Fake</td>
+                    <td>Contact</td>
+                    <td>Unknown Inc.</td>
+                    <td>000-0000</td>
+                    <td>fake@unknown.io</td>
+                    <td><Button>Edit</Button></td>
+                    <td><Button>Delete</Button></td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Mystery</td>
+                    <td>Contact</td>
+                    <td>Unknown Inc.</td>
+                    <td>000-0000</td>
+                    <td>mystery@unknown.io</td>
+                    <td><Button>Edit</Button></td>
+                    <td><Button>Delete</Button></td>
+                </tr>
+            </tbody>
+        </Table>)
+    }
 }
-
 export default ContactTable
